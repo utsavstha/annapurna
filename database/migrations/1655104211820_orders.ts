ffd24b14f801ba1bb94cd76a9874ff1792d3ef34
Utsav Shrestha <utsavstha@outlook.com>
2022-06-06 17:51:21 +0545

    new features

--- database/migrations/1655104211820_orders.ts
+++ database/migrations/1655104211820_orders.ts
@@ -14,9 +14,7 @@ export default class Orders extends BaseSchema {
         .unsigned()
         .references('tables.id')
         .onDelete('CASCADE')
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
+      
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
