df59586b7b5416853c26be009b2f2be35baef65c
Utsav Shrestha <utsavstha@outlook.com>
2022-06-14 06:52:35 +0545

    fixed

--- database/migrations/1657079974982_users.ts
+++ database/migrations/1657079974982_users.ts
@@ -12,7 +12,6 @@ export default class UsersSchema extends BaseSchema {
       table.string('password', 180).notNullable()
       table.string('role', 100).notNullable()
       table.string('remember_me_token').nullable()
-
       table.timestamp('created_at', { useTz: true }).notNullable()
       table.timestamp('updated_at', { useTz: true }).notNullable()
     })
